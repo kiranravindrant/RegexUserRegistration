c2475a3e79c43c226281f155252691f254a2a88e
kiranravindrant <kiranravindrant@gmail.com>
2021-07-24 20:33:32 +0530

    [kiran]Added:First Name Validation

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+
+namespace RegexUserRegistration
+{
+    class Program
+    {
+        public static string namePattern = "^[A-Z]{1}[a-z]{2,}";
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=======User Registration=====");
+            Console.WriteLine("Enter your FirstName");
+            string firstname = Console.ReadLine();
+
+            Console.WriteLine(NameValidation(firstname));
+
+
+
+
+
+
+
+        }
+        static bool NameValidation(string name)
+        {
+            return Regex.IsMatch(name, namePattern);
+        }
+        
+
+
+
+
+    }
+}      
+
+
