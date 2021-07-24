772b42925d8ab25a9728aa5b2c35ca6b55226076
kiranravindrant <kiranravindrant@gmail.com>
2021-07-24 23:33:35 +0530

    [kiran]Updated password validation to hold atleast one number

--- Program.cs
+++ Program.cs
@@ -9,26 +9,26 @@ namespace RegexUserRegistration
         public static string namePattern = "^[A-Z]{1}[a-z]{2,}";
         public static string emailPattern = "^[A-z a-z]{3,}[.]?([a-z A-z 0-9]{3,})?[@][a-z A-z]{2,}[.]?[a-z]{2,}[.]?[a-z]{2,3}$";
         public static string mobileNumPattern = "^[9][1][ ][0-9]{10}$";
-        public static string passPattern = "^((?=.*[A - Z])(?=.{8,}))";                                
+        public static string passPattern = "^((?=.*[A - Z])(?=.*[0 - 9])(?=.{8,}))";                                
         static void Main(string[] args)
         {
-            //Console.WriteLine("=======User Registration=====");
-            //Console.WriteLine("Enter your FirstName");
-            //string firstname = Console.ReadLine();
+            Console.WriteLine("=======User Registration=====");
+            Console.WriteLine("Enter your FirstName");
+            string firstname = Console.ReadLine();
 
-            //Console.WriteLine(NameValidation(firstname));
+            Console.WriteLine(NameValidation(firstname));
 
-            //Console.WriteLine("Enter your LastName");
-            //string lastname = Console.ReadLine();
-            //Console.WriteLine(NameValidation(lastname));
+            Console.WriteLine("Enter your LastName");
+            string lastname = Console.ReadLine();
+            Console.WriteLine(NameValidation(lastname));
 
-            //Console.WriteLine("Enter Email");
-            //string email = Console.ReadLine();
-            //Console.WriteLine(EmailValidation(email));
+            Console.WriteLine("Enter Email");
+            string email = Console.ReadLine();
+            Console.WriteLine(EmailValidation(email));
 
-            //Console.WriteLine("Enter Mobile Number with Countrycode, followed by space");
-            //string mob = Console.ReadLine();
-            //Console.WriteLine(MobileNumValidation(mob));
+            Console.WriteLine("Enter Mobile Number with Countrycode, followed by space");
+            string mob = Console.ReadLine();
+            Console.WriteLine(MobileNumValidation(mob));
 
             Console.WriteLine("Enter a Password");
             string pass = Console.ReadLine();
